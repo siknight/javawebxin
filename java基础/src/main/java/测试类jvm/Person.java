542e83474ab381e50d155a3efb97f381ef9222e7
lisi <1786678583@qq.com>
2020-01-06 15:14:41 +0800

    java笔记更新

--- java基础/src/main/java/测试类jvm/Person.java
+++ java基础/src/main/java/测试类jvm/Person.java
@@ -2,6 +2,14 @@ package 测试类jvm;
 
 public class Person {
     public Person() {
+
+    }
+    public void soutthis(){
         System.out.println(this);
     }
+
+//    @Override  //放开后打印的是这内容，不放开打印的是地址
+//    public String toString() {
+//        return "Person{}";
+//    }
 }
--- java基础/src/main/java/测试类jvm/TestPerson.java
+++ java基础/src/main/java/测试类jvm/TestPerson.java
@@ -3,6 +3,10 @@ package 测试类jvm;
 public class TestPerson {
     public static void main(String[] args) {
         Person person = new Person();
+        System.out.println(person);
+        Person person1 =person;
+        System.out.println(person1);
+        person1.soutthis();
 
     }
 }
